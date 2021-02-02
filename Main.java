1b6cecdd43f4749f18a7a6b45a4614bbc05f0738
RazUzan1 <73605965+RazUzan1@users.noreply.github.com>
2021-02-02 21:29:35 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,118 +1,91 @@
 package com.company;
-
 public  class Main {
-
+    public static void main(String[] args) {
+        //  System.out.println(isPrime( 2) );
+        // prime();
+        //  firstPrimes();
+        //System.out.println(PerfectNumbers(8128));
+        //Perfects();
+    }
 
     //targil1
 
     int num;
 
-    public boolean isPrime() {
-        boolean isPrime = true;
-        if (num == 0 || num == 1) return false;
-        if (num == 2) {
-            return true;
+    public static boolean isPrime(int num) {
+        if (num == 1 || num == 0) {
+            return false;
         }
-        for (int i = 2; i <= Math.sqrt(num); i++) {
-            if (isPrime) {
-                if (num % i == 0) {
-                    isPrime = false;
-                } else {
-                    isPrime = true;
-                }
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                return false;
             }
         }
-        return isPrime;
+        return true;
     }
-
 
     //targil2
 
-    public void prime(int num) {
-
-        int i, j;
-        int count = 0;
-        for (i = 2; i <= 1000000; i++) {
-            boolean IsPrime = true;
-
-            for (j = 2; j < i; j++) {
-                if (i % j == 0) {
-                    IsPrime = false;
-                    break;
-                }
-            }
-            if (IsPrime) {
-                if (IsPrime = true) {
-                    count++;
-                }
-                if (count >= num) {
-                    break;
-                }
-                System.out.println(i + ",");
+    public static int prime() {
+        int i;
+        for (i = 2; i < 1000000; i++) {
+            if (isPrime(i) == true) {
+                System.out.println(i);
             }
         }
+        return i;
     }
+
 
     //targil3
 
-    public static void first100PrimeNumbers (String[] args) {
-        int i = 0;
-        int num = 0;
-        String primeNumbers = "";
+    public static void firstPrimes() {
+        int i, cnt = 0;
+        for (i = 2; i < 1000000; i++) {
+            if (isPrime(i)) {
+                cnt++;
+                System.out.println(i);
+            }
+            if (cnt == 100) {
+                break;
+            }
+        }
+    }
 
-        for (i = 1; i <= 100; i++) {
-            int counter = 0;
-            for (num = i; num >= 1; num--) {
-                if (i % num == 0) {
-                    counter += 1;
+
+        //targil 4
+        public static boolean PerfectNumbers(int n) {
+            int sum = 0;
+            for (int i = 1; i < n; i++) {
+                if (n % i == 0) {
+                    sum += i;
                 }
             }
-            if (counter == 2) {
-                primeNumbers = primeNumbers + i + " ";
-            }
-        }
-        System.out.println("Prime numbers from 1 to 100 are :");
-        System.out.println(primeNumbers);
+            if (sum == n) {
+                            return true;
+                          }
+            else {
+                  return false;
+                 }
+
     }
-
-
-    //targil 4
-
-    public static boolean PerfectNumber(int n) {
-        int sum = 0;
-        for (int i = 1; i < n; i++) {
-            if (n % i == 0) {
-                sum += i;
-            }
-        }
-        if (sum == n) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
 
     //targil 5
 
 
-    public static void main(String[] args) {
-
-        int i, number = 1000000, sum = 0, factor;
-        for (i = 1; i < number; i++) {
-            for (factor = 1; factor < number; factor++) {
-                if (i % factor == 0) {
-                    sum = sum + factor;
-                }
-                if (sum == i) {
-                    System.out.println(sum);
-                }
-                sum = 0;
-            }
-
-        }
-    }
+    public static void Perfects(){
+      int i;
+      for(i=0;i<1000000;i++){
+          if (PerfectNumbers(i) == true){
+              System.out.println(i);
+          }
+          }
+      }
 }
+
+
+
+
 
 
 
